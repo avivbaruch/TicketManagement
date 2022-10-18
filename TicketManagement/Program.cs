3bfefd0ac337c37acca028d6be559437373740d7
aviv baruch <avivbaruh@gmail.com>
2022-10-18 19:06:32 +0300

    Add project files.

---
+++ TicketManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicketManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime();
+            TicketManagement gl = new TicketManagement(3,date,30);
+        }
+    }
+}
---
+++ TicketManagement/TicketManagement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketManagement
+{
+    class TicketManagement
+    {
+        public int TicketNumber { get; set; }
+        public DateTime date { get; set; }
+        public int TicketPrice { get; set; }
+
+        public TicketManagement(int ticketNumber, DateTime date, int ticketPrice)
+        {
+            TicketNumber = ticketNumber;
+            this.date = date;
+            TicketPrice = ticketPrice;
+        }
+
+     
+    }
+
+}
