6550309c9874702adb21a1ea365eb8fb7d1d5ac6
aviv baruch <avivbaruh@gmail.com>
2022-10-23 17:19:36 +0300

    1

---
+++ TicketManagement/ReadMe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketManagement
+{
+    class ReadMe
+    {
+        //1. להוסיף מחלקה שתנהל את כל האירוע 
+        //2.להוסיף פונקציות של הוספה וביטול 
+    }
+}
